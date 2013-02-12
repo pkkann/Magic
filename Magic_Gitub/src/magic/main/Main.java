feed709c93879725849bfc68d230bcbf6de6b67b
Patrick <pkkann@gmail.com>
2013-02-12 18:26:16 +0100

    Sletted test graphics

--- Magic_Gitub/src/magic/main/Main.java
+++ Magic_Gitub/src/magic/main/Main.java
@@ -4,12 +4,6 @@
  */
 package magic.main;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import magic.testgraphics.Graphics;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.SlickException;
-
 /**
  *
  * @author tobi6053
@@ -17,14 +11,7 @@ import org.newdawn.slick.SlickException;
 public class Main {
 
     public Main() {
-        // Graphics test
-        try {
-            AppGameContainer app = new AppGameContainer(new Graphics());
-            app.setDisplayMode(1024, 768, false);
-            app.start();
-        } catch (SlickException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        System.out.println("Nothing to start");
     }
 
     /**
