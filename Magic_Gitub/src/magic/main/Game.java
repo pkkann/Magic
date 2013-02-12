d23620034d8be67806119ab3b91e33dd6c1bcb27
Patrick <pkkann@gmail.com>
2013-02-12 18:41:29 +0100

    Graphics

--- Magic_Gitub/src/magic/main/Game.java
+++ Magic_Gitub/src/magic/main/Game.java
@@ -4,20 +4,41 @@
  */
 package magic.main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import magic.graphics.GraphicsHandler;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 /**
  *
  * @author tobi6053
  */
-public class Main {
-
-    public Main() {
-        System.out.println("Nothing to start");
+public class Game {
+    
+    private final String gameTitle = "Magic";
+    
+    private AppGameContainer app;
+    
+    public Game() {
+        
+    }
+    
+    public void startGraphics() {
+        try {
+            app = new AppGameContainer(new GraphicsHandler(gameTitle));
+            app.setDisplayMode(1024, 768, false);
+            app.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Main();
+        Game game  = new Game();
+        game.startGraphics();
     }
 }
