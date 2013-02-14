08fd84bcc0d9eb154c408d196af345f6bbfebf88
Patrick <pkkann@gmail.com>
2013-02-14 22:49:15 +0100

    Graphics_test

---
+++ Magic_Gitub/src/magic/graphics/tileset/TileHandler.java
@@ -0,0 +1,14 @@
+
+package magic.graphics.tileset;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Patrick
+ */
+public class TileHandler {
+
+    private int img_WIDTH = 0;
+    private int img_HEIGHT = 0;
+}
